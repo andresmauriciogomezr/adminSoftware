1d60662bc16172304c2d255f11b4418fac8eb06a
andresmauriciogomezr <andresmauriciogomezr@gmail.com>
2016-11-28 06:14:50 -0500

    El primer commit

---
+++ admin/entorno/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\usuario\google~1\8semes~1\software\source\admin\entorno\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
